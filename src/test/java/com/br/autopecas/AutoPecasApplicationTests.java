6d1a1570483df7232eab2760dab2bdbccaa3d036
Erivelton Ribeiro <erivelton84ribeiro@gmail.com>
2026-03-10 15:48:23 -0300

    Iniciando backend.

---
+++ src/test/java/com/br/autopecas/AutoPecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.autopecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
